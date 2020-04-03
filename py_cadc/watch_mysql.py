75f793728c1d019d0c0b4f54d393bcf0a9bff55b
Luis Manuel Torres Trevino <torrestrevino..luis@gmail.com>
2020-04-03 17:04:05 -0600

    [IMP]reformating watch_mysql.py file

--- py_cadc/watch_mysql.py
+++ py_cadc/watch_mysql.py
@@ -1,11 +1,6 @@
 from pymysqlreplication import BinLogStreamReader
 
-mysql_settings = {
-    'host': '127.0.0.1',
-    'port': 3308,
-    'user': 'root',
-    'passwd': 'cain123'
-}
+mysql_settings = {"host": "127.0.0.1", "port": 3308, "user": "root", "passwd": "cain123"}
 
 stream = BinLogStreamReader(connection_settings=mysql_settings, server_id=1)
 
