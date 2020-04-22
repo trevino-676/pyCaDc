8b01ca4443a84f68bb5c6912fc2b2b135ea302f9
Luis Manuel Torres Trevino <torrestrevino..luis@gmail.com>
2020-04-21 19:39:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ with open("README.md", 'r') as fh:
 
 setuptools.setup(
     name='pycacd',
-    version="0.4.0",
+    version="0.4.1",
     author='Luis Manuel Torres Trevino',
     author_email='lmtorres123@icloud.com',
     description='Function library to track changes in SQL server and MySQL databases.',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/trevino-676/pyCaDc",
     packages=['py_cadc'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
